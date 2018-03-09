26fecbf4686cd81df08a582ac5ef3c820a3d5e3f
Aleksandr <syaskiy9523@gmail.com>
2018-03-09 17:33:03 +0000

    bootstrap

--- js/index.js
+++ js/index.js
@@ -1,5 +1,28 @@
+function myClock() {
+ let siteTime = new Date();
+ let today = 16 - siteTime.getDate() ;
+ let hour = 24 - siteTime.getHours() ;
+ let minute =  60 - siteTime.getMinutes()  ;
+ let second = 60 - siteTime.getSeconds();
+ if (today < 10) today = "0" + today;
+ if (hour < 10) hour = "0" + hour;
+ if (minute < 10) minute = "0" + minute;
+ if (second < 10) second = "0" + second;
 
+ if(today <= 0  && hour <= 0 &&  minute  <= 0   &&  second == 30)
+ {
+ document.getElementById("siteTime").innerHTML = "Час вийшов";
+ clearInterval(interval);
 
- function changeImg(source){
-  document.pic.src = source + '.png';
  }
+ else{
+ document.getElementById("day").innerHTML = today;
+ document.getElementById("hour").innerHTML = hour;
+ document.getElementById("minute").innerHTML = minute;
+ document.getElementById("second").innerHTML = second;
+ document.getElementById("siteTime").innerHTML = today + ":" + hour + ":" + minute + ":" + second;
+}
+  
+ }
+
+let interval = setInterval(myClock, 1000);
